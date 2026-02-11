c7bf3fdd229b8b90632ce55387a76920add0c0c3
Jesse Sher <jebba272727@gmail.com>
2026-02-11 23:11:59 +1100

    did stuf

---
+++ app/build/generated/source/buildConfig/debug/com/raylib/raymob/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.raylib.raymob;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.raylib.raymob";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final boolean FEATURE_DISPLAY_IMMERSIVE = true;
+  // Field from default config.
+  public static final boolean FEATURE_DISPLAY_INTO_CUTOUT = true;
+  // Field from default config.
+  public static final boolean FEATURE_DISPLAY_KEEP_ON = true;
+}
---
+++ app/build/intermediates/cxx/Debug/5ry1e636/logs/arm64-v8a/build_command_raymob.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\jebba\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\jebba\\StudioProjects\\RaylibAndroidBallTest\\app\\.cxx\\Debug\\5ry1e636\\arm64-v8a" ^
+  raymob
